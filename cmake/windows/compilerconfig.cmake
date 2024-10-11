c911149de22f5ab4b0564f03c75541890be71285
jcm <6864788+jcm93@users.noreply.github.com>
2024-10-11 13:24:09 -0600

    windows arch config, clang-cl options

--- cmake/windows/compilerconfig.cmake
+++ cmake/windows/compilerconfig.cmake
@@ -7,6 +7,19 @@ include_guard(GLOBAL)
 include(ccache)
 include(compiler_common)
 
+if(CMAKE_GENERATOR_PLATFORM)
+  set(arch ${CMAKE_GENERATOR_PLATFORM})
+  set(platform windows-${arch})
+else()
+  if(CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL ARM64)
+    set(arch arm64)
+    set(platform windows-${arch})
+  else()
+    set(arch x64)
+    set(platform windows-${arch})
+  endif()
+endif()
+
 add_compile_definitions(_WIN32_WINNT=0x0601) #global
 
 set(
@@ -22,24 +35,29 @@ set(
 )
 
 if(MSVC)
-  add_compile_options(
-    "$<$<COMPILE_LANGUAGE:C,CXX>:${_ares_msvc_cxx_options}>"
-  )
-  add_link_options(
-    $<$<NOT:$<CONFIG:Debug>>:/LTCG>
-    $<$<NOT:$<CONFIG:Debug>>:/INCREMENTAL:NO>
-    /Debug
-  )
-  set(CMAKE_MSVC_DEBUG_INFORMATION_FORMAT ProgramDatabase)
-  if(CMAKE_COMPILE_WARNING_AS_ERROR)
-    add_link_options(/WX)
+  if(CMAKE_CXX_COMPILER_ID STREQUAL MSVC)
+    add_compile_options(
+      "$<$<COMPILE_LANGUAGE:C,CXX>:${_ares_msvc_cxx_options}>"
+    )
+    add_link_options(
+      $<$<NOT:$<CONFIG:Debug>>:/LTCG>
+      $<$<NOT:$<CONFIG:Debug>>:/INCREMENTAL:NO>
+      /Debug
+    )
+    if(CMAKE_COMPILE_WARNING_AS_ERROR)
+      add_link_options(/WX)
+    endif()
+  elseif(CMAKE_CXX_COMPILER_ID STREQUAL Clang)
+    add_compile_options(
+      "$<$<COMPILE_LANGUAGE:C>:${_ares_clang_c_options}>"
+      "$<$<COMPILE_LANGUAGE:CXX>:${_ares_clang_cxx_options}>"
+    )
+    -Wno-reorder-ctor
+    -Wno-unused
   endif()
+  set(CMAKE_MSVC_DEBUG_INFORMATION_FORMAT ProgramDatabase)
 else()
-  add_compile_options(
-    "$<$<COMPILE_LANGUAGE:C>:${_ares_clang_c_options}>"
-    "$<$<COMPILE_LANGUAGE:CXX>:${_ares_clang_cxx_options}>"
-  )
-  # any ms-specific clang things?
+  # msys2
 endif()
 
 if(ARES_BUILD_LOCAL)
