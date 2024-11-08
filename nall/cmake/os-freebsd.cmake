ef8410f058feba133e7e63df62fd65856c24c2f5
jcm <6864788+jcm93@users.noreply.github.com>
2024-11-08 13:06:02 -0600

    Pull in FreeBSD support
    Co-authored-by: Stefan Schlosser <bsdcode@disroot.org>

--- nall/cmake/os-freebsd.cmake
+++ nall/cmake/os-freebsd.cmake
@@ -1,1 +1,21 @@
+target_compile_definitions(nall PUBLIC PLATFORM_BSD)
 
+target_sources(
+  nall
+  PRIVATE # cmake-format: sortable
+    xorg/clipboard.hpp
+    xorg/guard.hpp
+    xorg/xorg.hpp
+)
+
+if(DEFINED ARES_PREFIX)
+
+  target_compile_definitions(nall PRIVATE "ARES_PREFIX=${ARES_PREFIX}")
+  
+  install(CODE "
+    if(NOT \"${CMAKE_INSTALL_PREFIX}\" STREQUAL \"${ARES_PREFIX}\")
+      message(FATAL_ERROR \" nall was configured and built with the data directory prefix ${ARES_PREFIX}, but the chosen CMake install prefix, ${CMAKE_INSTALL_PREFIX}, does not match. This is considered an error because it makes it unlikely for the program to properly locate shared resources in the shared data directory.\")
+    endif()
+  ")
+  
+endif()
