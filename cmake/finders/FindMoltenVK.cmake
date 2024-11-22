894cc4a4ced2c9e839878c2ceae77835f449bcdd
jcm <6864788+jcm93@users.noreply.github.com>
2024-11-22 14:50:27 -0600

    more find module cleanup

--- cmake/finders/FindMoltenVK.cmake
+++ cmake/finders/FindMoltenVK.cmake
@@ -43,43 +43,8 @@ if(PKG_CONFIG_FOUND)
   pkg_search_module(PC_MoltenVK MoltenVK)
 endif()
 
-# MoltenVK_set_soname: Set SONAME on imported library target
-macro(MoltenVK_set_soname)
-  if(CMAKE_HOST_SYSTEM_NAME MATCHES "Darwin")
-    execute_process(
-      COMMAND sh -c "otool -D '${MoltenVK_LIBRARY}' | grep -v '${MoltenVK_LIBRARY}'"
-      OUTPUT_VARIABLE _output
-      RESULT_VARIABLE _result
-    )
-
-    if(_result EQUAL 0 AND _output MATCHES "^@rpath/")
-      set_property(TARGET MoltenVK::MoltenVK PROPERTY IMPORTED_SONAME "${_output}")
-    endif()
-  elseif(CMAKE_HOST_SYSTEM_NAME MATCHES "Linux|FreeBSD")
-    execute_process(
-      COMMAND sh -c "objdump -p '${MoltenVK_LIBRARY}' | grep SONAME"
-      OUTPUT_VARIABLE _output
-      RESULT_VARIABLE _result
-    )
-
-    if(_result EQUAL 0)
-      string(REGEX REPLACE "[ \t]+SONAME[ \t]+([^ \t]+)" "\\1" _soname "${_output}")
-      set_property(TARGET MoltenVK::MoltenVK PROPERTY IMPORTED_SONAME "${_soname}")
-      unset(_soname)
-    endif()
-  endif()
-  unset(_output)
-  unset(_result)
-endmacro()
-
 if(PC_MoltenVK_VERSION VERSION_GREATER 0)
   set(MoltenVK_VERSION ${PC_MoltenVK_VERSION})
-elseif(EXISTS "${MoltenVK_INCLUDE_DIR}/version.h")
-  file(STRINGS "${_VERSION_FILE}" _VERSION_STRING REGEX "^.*VERSION_(MAJOR|MINOR|PATCH)[ \t]+[0-9]+[ \t]*$")
-  string(REGEX REPLACE ".*VERSION_MAJOR[ \t]+([0-9]+).*" "\\1" _VERSION_MAJOR "${_VERSION_STRING}")
-  string(REGEX REPLACE ".*VERSION_MINOR[ \t]+([0-9]+).*" "\\1" _VERSION_MINOR "${_VERSION_STRING}")
-  string(REGEX REPLACE ".*VERSION_PATCH[ \t]+([0-9]+).*" "\\1" _VERSION_PATCH "${_VERSION_STRING}")
-  set(MoltenVK_VERSION "${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_MICRO}")
 else()
   if(NOT MoltenVK_FIND_QUIETLY)
     message(AUTHOR_WARNING "Failed to find MoltenVK version.")
@@ -119,8 +84,6 @@ if(MoltenVK_FOUND)
       add_library(MoltenVK::MoltenVK SHARED IMPORTED)
       set_property(TARGET MoltenVK::MoltenVK PROPERTY IMPORTED_LIBNAME "${MoltenVK_LIBRARY}")
     endif()
-
-    moltenvk_set_soname()
     set_target_properties(
       MoltenVK::MoltenVK
       PROPERTIES INTERFACE_COMPILE_OPTIONS "${PC_MoltenVK_CFLAGS_OTHER}" VERSION ${MoltenVK_VERSION}
