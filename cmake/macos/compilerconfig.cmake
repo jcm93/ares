e6003e90b6c10302e99caa3691c364e92b4060cf
jcm <6864788+jcm93@users.noreply.github.com>
2024-12-20 18:08:18 -0600

    re-add linker optimizations removed by /debug

--- cmake/macos/compilerconfig.cmake
+++ cmake/macos/compilerconfig.cmake
@@ -32,6 +32,12 @@ unset(_ares_macos_current_sdk)
 unset(_ares_macos_minimum_sdk)
 unset(_ares_macos_minimum_xcode)
 
+message(AUTHOR_WARNING "cmake reldeb flags are ${CMAKE_CXX_FLAGS_RELWITHDEBINFO}")
+message(AUTHOR_WARNING "cmake reldeb link flags are ${CMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO}")
+message(AUTHOR_WARNING "cmake reldeb link flags are ${CMAKE_SHARED_LINKER_FLAGS_RELWITHDEBINFO}")
+message(AUTHOR_WARNING "cmake reldeb link flags are ${CMAKE_MODULE_LINKER_FLAGS_RELWITHDEBINFO}")
+
+
 # Enable dSYM generator for release builds
 string(APPEND CMAKE_C_FLAGS_RELEASE " -g")
 string(APPEND CMAKE_CXX_FLAGS_RELEASE " -g")
