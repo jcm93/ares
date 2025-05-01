a47634449f77551a37939d991e5029538371e064
jcm <6864788+jcm93@users.noreply.github.com>
2025-04-30 23:45:03 -0500

    toolbar and more UI changes

---
+++ hiro/cocoa/widget/toolbar-item.cpp
@@ -0,0 +1,45 @@
+#if defined(Hiro_Toolbar)
+
+namespace hiro {
+
+auto pToolbarItem::construct() -> void {
+}
+
+auto pToolbarItem::destruct() -> void {
+}
+
+auto pToolbarItem::append(sSizable sizable) -> void {
+}
+
+auto pToolbarItem::remove(sSizable sizable) -> void {
+}
+
+auto pToolbarItem::setClosable(bool closable) -> void {
+}
+
+auto pToolbarItem::setIcon(const image& icon) -> void {
+}
+
+auto pToolbarItem::setMovable(bool movable) -> void {
+}
+
+auto pToolbarItem::setSelected() -> void {
+  if(auto parent = _parent()) {
+    [(CocoaToolbar*)(parent->cocoaView) selectTabViewItem:cocoaToolbarItem];
+  }
+}
+
+auto pToolbarItem::setText(const string& text) -> void {
+  [cocoaToolbarItem setLabel:[NSString stringWithUTF8String:state().text]];
+}
+
+auto pToolbarItem::_parent() -> maybe<pToolbar&> {
+  if(auto parent = self().parentToolbar()) {
+    if(auto self = parent->self()) return *self;
+  }
+  return nothing;
+}
+
+}
+
+#endif
